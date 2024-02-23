41911595056a8ddeabb572269aac649371c24926
tczsoft <vinayagam.mgb@gmail.com>
2024-02-23 19:02:00 +0530

    n

--- assets/JS/fuction.js
+++ assets/JS/fuction.js
@@ -2,3 +2,10 @@ function myFunction() {
     let menu = document.getElementById("navbar").classList.contains('hidden');
     menu ? document.getElementById("navbar").classList.remove('hidden') : document.getElementById("navbar").classList.add('hidden');
 }
+
+function myModel() {
+    console.log('hiiii')
+    let menu = document.getElementById("modal").classList.contains('hidden');
+    console.log(menu)
+    menu ? document.getElementById("modal").classList.remove('hidden') : document.getElementById("modal").classList.add('hidden');
+}
